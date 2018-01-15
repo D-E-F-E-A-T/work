fa38651c2e7941830800bc5fd50a447ad1c50ce2
Bryan Tran <b0t0097@homeoffice.wal-mart.com>
2018-01-15 13:51:19 -0600

    saved a few files, beginning react files

--- youtube-search/src/index.js
+++ youtube-search/src/index.js
@@ -1,15 +1,15 @@
+// Import React from the node modules (npm)
+// note: we use react-dom to render component to dom!
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This component should produce some HTML
+// Creating a constant called App and assigning it a function that produces HTML
+// We write in JSX when writing a component because it's clean and concise
+const App = function() {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// When we make a component we want to make sure it is inserted into the DOM (page)
+// USE THE REACTDOM library!
+ReactDOM.render(App);
