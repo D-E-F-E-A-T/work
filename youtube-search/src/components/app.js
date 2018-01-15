c387138ab983e50f80a36f981e9e06ca347f0321
Bryan Tran <b0t0097@homeoffice.wal-mart.com>
2018-01-15 12:46:55 -0600

    first commit for practice work

---
+++ youtube-search/src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; // Importing react components
+
+// exports whatever is in here
+export default class App extends Component {
+  // render whatever in here
+  render() {
+    return (
+      <div>Hello there!</div>
+    );
+  }
+}
